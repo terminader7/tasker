7c804bc681f874b1a7b026ad5a84d5f44296a7b6
unknown <nebrahim2424@gmail.com>
2025-02-20 14:00:13 -0800

    revmoed unused code

--- frontend/src/contexts/projectContext.tsx
+++ frontend/src/contexts/projectContext.tsx
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
 import { Project } from "../types/project";
 import { getProject } from "../api/projectService";
 
